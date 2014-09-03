41d97e83fa19a9a88e4587ae7f53b4b08d971406
cxfksword <cxfksword@gmail.com>
2014-09-04 00:59:39 +0800

    Change to use font owersome

--- public/js/site.js
+++ public/js/site.js
@@ -1,61 +1,104 @@
+
 function notify_info(msg) {
-    new PNotify({
+    var option = {
             text: msg,
             animate_speed: 'fast',
             stack: false,
             delay: 1000,
+            hide: true,
             type: 'info',
+            width: "150px",
+            icon: 'fa fa-info-circle',
             before_open: function(PNotify){
                   PNotify.get().css({
                     "top": ($(window).height() / 2) - (PNotify.get().height() / 2) - 100,
                     "left": ($(window).width() / 2) - (PNotify.get().width() / 2)
                   });
             }
-    });
+    };
+
+    show_notify(option);
+}
+function show_notify(option) {
+    if (typeof pnotify == 'undefined') {
+        pnotify = new PNotify( option);
+    } else {
+        pnotify.update(option);
+        if (option.hide) {
+            pnotify = undefined;
+        }
+    }
 }
 function notify_error (msg) {
-    new PNotify({
+    var option = {
             text: msg,
             animate_speed: 'fast',
             stack: false,
             delay: 1000,
+            hide: true,
             type: 'error',
-            width: "200px",
+            width: "150px",
+            icon: 'fa fa-warning',
             before_open: function(PNotify){
                   PNotify.get().css({
                     "top": ($(window).height() / 2) - (PNotify.get().height() / 2) - 100,
                     "left": ($(window).width() / 2) - (PNotify.get().width() / 2)
                   });
                 }
-    });
+    };
+    show_notify(option);
 }
 function notify_success (msg) {
-    new PNotify({
+    var option = {
             text: msg,
             animate_speed: 'fast',
             stack: false,
             delay: 1000,
+            hide: true,
             type: 'success',
+            width: "150px",
+            icon: 'fa fa-check-circle',
             before_open: function(PNotify){
                   PNotify.get().css({
                     "top": ($(window).height() / 2) - (PNotify.get().height() / 2) - 100,
                     "left": ($(window).width() / 2) - (PNotify.get().width() / 2)
                   });
                 }
-    });
+    };
+    show_notify(option);
 }
-function notify(msg) {
-    new PNotify({
+function notify_loading(msg) {
+    var option = {
             text: msg,
             animate_speed: 'fast',
             stack: false,
-            delay: 1000,
+            hide: false,
             width: "150px",
+            icon: 'fa fa-cog fa-spin',
             before_open: function(PNotify){
                   PNotify.get().css({
                     "top": ($(window).height() / 2) - (PNotify.get().height() / 2) - 100,
                     "left": ($(window).width() / 2) - (PNotify.get().width() / 2)
                   });
              }
-    });
+    };
+    show_notify(option);
+}
+function notify(msg) {
+    var option = {
+            text: msg,
+            animate_speed: 'fast',
+            stack: false,
+            delay: 1000,
+            hide: true,
+            width: "150px",
+            icon: 'fa fa-exclamation-circle',
+            before_open: function(PNotify){
+                  PNotify.get().css({
+                    "top": ($(window).height() / 2) - (PNotify.get().height() / 2) - 100,
+                    "left": ($(window).width() / 2) - (PNotify.get().width() / 2)
+                  });
+             }
+    };
+    show_notify(option);
 }
